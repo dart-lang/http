971d1ed23f374bcb59c55609bf4752e11b03475d
Don <don.j.olmstead@gmail.com>
2017-01-16 07:58:54 -0800

    Remove `@override` annotation

--- lib/src/request.dart
+++ lib/src/request.dart
@@ -78,7 +78,6 @@ class Request extends Message {
   ///
   /// [body] is the request body. It may be either a [String], a [List<int>], a
   /// [Stream<List<int>>], or `null` to indicate no body.
-  @override
   Request change(
       {Map<String, String> headers,
       Map<String, Object> context,
