849aa3e4a5f9eb195d476d08cbaf70e902601202
Yegor <yjbanov@google.com>
2017-03-29 17:29:45 -0700

    remove boundary characters that pkg:http_parser cannot parse (#72)

--- lib/src/multipart_request.dart
+++ lib/src/multipart_request.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'base_request.dart';
+import 'boundary_characters.dart';
 import 'byte_stream.dart';
 import 'multipart_file.dart';
 import 'utils.dart';
@@ -117,21 +118,6 @@ class MultipartRequest extends BaseRequest {
     return new ByteStream(controller.stream);
   }
 
-  /// All character codes that are valid in multipart boundaries. This is the
-  /// intersection of the characters allowed in the `bcharsnospace` production
-  /// defined in [RFC 2046][] and those allowed in the `token` production
-  /// defined in [RFC 1521][].
-  ///
-  /// [RFC 2046]: http://tools.ietf.org/html/rfc2046#section-5.1.1.
-  /// [RFC 1521]: https://tools.ietf.org/html/rfc1521#section-4
-  static const List<int> _BOUNDARY_CHARACTERS = const <int>[
-    39, 43, 95, 44, 45, 46, 58, 61, 63, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-    65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
-    84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106,
-    107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
-    122
-  ];
-
   /// Returns the header string for a field. The return value is guaranteed to
   /// contain only ASCII characters.
   String _headerForField(String name, String value) {
@@ -172,7 +158,7 @@ class MultipartRequest extends BaseRequest {
     var prefix = "dart-http-boundary-";
     var list = new List<int>.generate(_BOUNDARY_LENGTH - prefix.length,
         (index) =>
-            _BOUNDARY_CHARACTERS[_random.nextInt(_BOUNDARY_CHARACTERS.length)],
+            BOUNDARY_CHARACTERS[_random.nextInt(BOUNDARY_CHARACTERS.length)],
         growable: false);
     return "$prefix${new String.fromCharCodes(list)}";
   }
