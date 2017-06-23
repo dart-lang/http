f32c5aae7890d4b36821dfe7e69f9c216225faa9
Natalie Weizenbaum <nex342@gmail.com>
2017-06-23 13:21:37 -0700

    Style change.

--- lib/src/pipeline.dart
+++ lib/src/pipeline.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+k// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -16,6 +16,7 @@ import 'middleware.dart';
 class Pipeline {
   /// The outer pipeline.
   final Pipeline _parent;
+
   /// The [Middleware] that is invoked at this stage.
   final Middleware _middleware;
 
