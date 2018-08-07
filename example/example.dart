9e2de1ee197d24627b68297f5d88e863d471a3ff
Don <don.j.olmstead@gmail.com>
2018-08-07 08:20:12 -0700

    Add simple example

---
+++ example/example.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:http/http.dart' as http;
+
+main() async {
+  var url = 'https://httpbin.org/post';
+  var response = await http.post(url, <String, String>{
+    'name': 'doodle',
+    'color': 'blue',
+  });
+  var responseText = await response.readAsString();
+
+  print(response.statusCode);
+  print(responseText);
+}
