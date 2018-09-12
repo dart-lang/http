6880dca213e55c9ba09ed3125bf9c869f132ea6b
Jacob MacDonald <jakemac@google.com>
2018-09-12 08:28:53 -0700

    only export IoClient from io_client.dart

--- lib/io_client.dart
+++ lib/io_client.dart
@@ -2,4 +2,4 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/io_client.dart';
+export 'src/io_client.dart' show IOClient;
