7e0bd385931baf4ade5f70ebc13afb3ad189f6ff
Michael Thomsen <mit-mit@users.noreply.github.com>
2019-01-02 13:49:19 +0100

    Review feedback

--- example/main.dart
+++ example/main.dart
@@ -1,19 +1,18 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-main(List<String> arguments) {
+main(List<String> arguments) async {
   // This example uses the Google Books API to search for books about http.
   // https://developers.google.com/books/docs/overview
   var url = "https://www.googleapis.com/books/v1/volumes?q={http}";
 
   // Await the http get response, then decode the json-formatted responce.
-  http.get(url).then((response) {
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      var itemCount = jsonResponse['totalItems'];
-      print("Number of books about http: $itemCount.");
-    } else {
-      print("Request failed with status: ${response.statusCode}.");
-    }
-  });
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    var itemCount = jsonResponse['totalItems'];
+    print("Number of books about http: $itemCount.");
+  } else {
+    print("Request failed with status: ${response.statusCode}.");
+  }
 }
