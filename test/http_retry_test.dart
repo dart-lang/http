3e307fe7926f1d84582cffe27c5ee40678696a08
Kevin Moore <kevmoo@google.com>
2019-05-05 17:14:32 -0700

    Test on oldest supported SDK, enable and fix many lints

--- test/http_retry_test.dart
+++ test/http_retry_test.dart
@@ -5,32 +5,29 @@
 import 'package:fake_async/fake_async.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
-import 'package:test/test.dart';
-
 import 'package:http_retry/http_retry.dart';
+import 'package:test/test.dart';
 
 void main() {
   group("doesn't retry when", () {
     test("a request has a non-503 error code", () async {
-      var client = new RetryClient(new MockClient(
-          expectAsync1((_) async => new Response("", 502), count: 1)));
+      var client = RetryClient(
+          MockClient(expectAsync1((_) async => Response("", 502), count: 1)));
       var response = await client.get("http://example.org");
       expect(response.statusCode, equals(502));
     });
 
     test("a request doesn't match when()", () async {
-      var client = new RetryClient(
-          new MockClient(
-              expectAsync1((_) async => new Response("", 503), count: 1)),
+      var client = RetryClient(
+          MockClient(expectAsync1((_) async => Response("", 503), count: 1)),
           when: (_) => false);
       var response = await client.get("http://example.org");
       expect(response.statusCode, equals(503));
     });
 
     test("retries is 0", () async {
-      var client = new RetryClient(
-          new MockClient(
-              expectAsync1((_) async => new Response("", 503), count: 1)),
+      var client = RetryClient(
+          MockClient(expectAsync1((_) async => Response("", 503), count: 1)),
           retries: 0);
       var response = await client.get("http://example.org");
       expect(response.statusCode, equals(503));
@@ -39,10 +36,10 @@ void main() {
 
   test("retries on a 503 by default", () async {
     var count = 0;
-    var client = new RetryClient(
-        new MockClient(expectAsync1((request) async {
+    var client = RetryClient(
+        MockClient(expectAsync1((request) async {
           count++;
-          return count < 2 ? new Response("", 503) : new Response("", 200);
+          return count < 2 ? Response("", 503) : Response("", 200);
         }, count: 2)),
         delay: (_) => Duration.zero);
 
@@ -52,10 +49,10 @@ void main() {
 
   test("retries on any request where when() returns true", () async {
     var count = 0;
-    var client = new RetryClient(
-        new MockClient(expectAsync1((request) async {
+    var client = RetryClient(
+        MockClient(expectAsync1((request) async {
           count++;
-          return new Response("", 503,
+          return Response("", 503,
               headers: {"retry": count < 2 ? "true" : "false"});
         }, count: 2)),
         when: (response) => response.headers["retry"] == "true",
@@ -68,13 +65,14 @@ void main() {
 
   test("retries on any request where whenError() returns true", () async {
     var count = 0;
-    var client = new RetryClient(
-        new MockClient(expectAsync1((request) async {
+    var client = RetryClient(
+        MockClient(expectAsync1((request) async {
           count++;
-          if (count < 2) throw "oh no";
-          return new Response("", 200);
+          if (count < 2) throw StateError("oh no");
+          return Response("", 200);
         }, count: 2)),
-        whenError: (error, _) => error == "oh no",
+        whenError: (error, _) =>
+            error is StateError && error.message == "oh no",
         delay: (_) => Duration.zero);
 
     var response = await client.get("http://example.org");
@@ -82,27 +80,26 @@ void main() {
   });
 
   test("doesn't retry a request where whenError() returns false", () async {
-    var client = new RetryClient(
-        new MockClient(expectAsync1((request) async => throw "oh no")),
+    var client = RetryClient(
+        MockClient(expectAsync1((request) async => throw StateError("oh no"))),
         whenError: (error, _) => error == "oh yeah",
         delay: (_) => Duration.zero);
 
-    expect(client.get("http://example.org"), throwsA("oh no"));
+    expect(client.get("http://example.org"),
+        throwsA(isStateError.having((e) => e.message, 'message', "oh no")));
   });
 
   test("retries three times by default", () async {
-    var client = new RetryClient(
-        new MockClient(
-            expectAsync1((_) async => new Response("", 503), count: 4)),
+    var client = RetryClient(
+        MockClient(expectAsync1((_) async => Response("", 503), count: 4)),
         delay: (_) => Duration.zero);
     var response = await client.get("http://example.org");
     expect(response.statusCode, equals(503));
   });
 
   test("retries the given number of times", () async {
-    var client = new RetryClient(
-        new MockClient(
-            expectAsync1((_) async => new Response("", 503), count: 13)),
+    var client = RetryClient(
+        MockClient(expectAsync1((_) async => Response("", 503), count: 13)),
         retries: 12,
         delay: (_) => Duration.zero);
     var response = await client.get("http://example.org");
@@ -110,87 +107,82 @@ void main() {
   });
 
   test("waits 1.5x as long each time by default", () {
-    new FakeAsync().run((fake) {
+    FakeAsync().run((fake) {
       var count = 0;
-      var client = new RetryClient(new MockClient(expectAsync1((_) async {
+      var client = RetryClient(MockClient(expectAsync1((_) async {
         count++;
         if (count == 1) {
           expect(fake.elapsed, equals(Duration.zero));
         } else if (count == 2) {
-          expect(fake.elapsed, equals(new Duration(milliseconds: 500)));
+          expect(fake.elapsed, equals(Duration(milliseconds: 500)));
         } else if (count == 3) {
-          expect(fake.elapsed, equals(new Duration(milliseconds: 1250)));
+          expect(fake.elapsed, equals(Duration(milliseconds: 1250)));
         } else if (count == 4) {
-          expect(fake.elapsed, equals(new Duration(milliseconds: 2375)));
+          expect(fake.elapsed, equals(Duration(milliseconds: 2375)));
         }
 
-        return new Response("", 503);
+        return Response("", 503);
       }, count: 4)));
 
       expect(client.get("http://example.org"), completes);
-      fake.elapse(new Duration(minutes: 10));
+      fake.elapse(Duration(minutes: 10));
     });
   });
 
   test("waits according to the delay parameter", () {
-    new FakeAsync().run((fake) {
+    FakeAsync().run((fake) {
       var count = 0;
-      var client = new RetryClient(
-          new MockClient(expectAsync1((_) async {
+      var client = RetryClient(
+          MockClient(expectAsync1((_) async {
             count++;
             if (count == 1) {
               expect(fake.elapsed, equals(Duration.zero));
             } else if (count == 2) {
               expect(fake.elapsed, equals(Duration.zero));
             } else if (count == 3) {
-              expect(fake.elapsed, equals(new Duration(seconds: 1)));
+              expect(fake.elapsed, equals(Duration(seconds: 1)));
             } else if (count == 4) {
-              expect(fake.elapsed, equals(new Duration(seconds: 3)));
+              expect(fake.elapsed, equals(Duration(seconds: 3)));
             }
 
-            return new Response("", 503);
+            return Response("", 503);
           }, count: 4)),
-          delay: (requestCount) => new Duration(seconds: requestCount));
+          delay: (requestCount) => Duration(seconds: requestCount));
 
       expect(client.get("http://example.org"), completes);
-      fake.elapse(new Duration(minutes: 10));
+      fake.elapse(Duration(minutes: 10));
     });
   });
 
   test("waits according to the delay list", () {
-    new FakeAsync().run((fake) {
+    FakeAsync().run((fake) {
       var count = 0;
-      var client = new RetryClient.withDelays(
-          new MockClient(expectAsync1((_) async {
+      var client = RetryClient.withDelays(
+          MockClient(expectAsync1((_) async {
             count++;
             if (count == 1) {
               expect(fake.elapsed, equals(Duration.zero));
             } else if (count == 2) {
-              expect(fake.elapsed, equals(new Duration(seconds: 1)));
+              expect(fake.elapsed, equals(Duration(seconds: 1)));
             } else if (count == 3) {
-              expect(fake.elapsed, equals(new Duration(seconds: 61)));
+              expect(fake.elapsed, equals(Duration(seconds: 61)));
             } else if (count == 4) {
-              expect(fake.elapsed, equals(new Duration(seconds: 73)));
+              expect(fake.elapsed, equals(Duration(seconds: 73)));
             }
 
-            return new Response("", 503);
+            return Response("", 503);
           }, count: 4)),
-          [
-            new Duration(seconds: 1),
-            new Duration(minutes: 1),
-            new Duration(seconds: 12)
-          ]);
+          [Duration(seconds: 1), Duration(minutes: 1), Duration(seconds: 12)]);
 
       expect(client.get("http://example.org"), completes);
-      fake.elapse(new Duration(minutes: 10));
+      fake.elapse(Duration(minutes: 10));
     });
   });
 
   test("calls onRetry for each retry", () async {
     var count = 0;
-    var client = new RetryClient(
-        new MockClient(
-            expectAsync1((_) async => new Response("", 503), count: 3)),
+    var client = RetryClient(
+        MockClient(expectAsync1((_) async => Response("", 503), count: 3)),
         retries: 2,
         delay: (_) => Duration.zero,
         onRetry: expectAsync3((request, response, retryCount) {
@@ -204,8 +196,8 @@ void main() {
   });
 
   test("copies all request attributes for each attempt", () async {
-    var client = new RetryClient.withDelays(
-        new MockClient(expectAsync1((request) async {
+    var client = RetryClient.withDelays(
+        MockClient(expectAsync1((request) async {
           expect(request.contentLength, equals(5));
           expect(request.followRedirects, isFalse);
           expect(request.headers, containsPair("foo", "bar"));
@@ -214,11 +206,11 @@ void main() {
           expect(request.persistentConnection, isFalse);
           expect(request.url, equals(Uri.parse("http://example.org")));
           expect(request.body, equals("hello"));
-          return new Response("", 503);
+          return Response("", 503);
         }, count: 2)),
         [Duration.zero]);
 
-    var request = new Request("POST", Uri.parse("http://example.org"));
+    var request = Request("POST", Uri.parse("http://example.org"));
     request.body = "hello";
     request.followRedirects = false;
     request.headers["foo"] = "bar";
