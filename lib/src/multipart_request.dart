0cef5a64c3a2ce67d63694ba90f2260ea9f8b090
Nate Bosch <nbosch@google.com>
2020-01-03 12:14:34 -0800

    Synchronously finalize headers
    Fixes #351
    
    An `async*` method is not started until after the stream has a listener,
    if a caller reads the headers before listening to the stream, they would
    see the value before they have been set. Move the header assignment and
    other finalization work to the wrapping method and keep the `async*`
    method handling only the stream content.

--- lib/src/multipart_request.dart
+++ lib/src/multipart_request.dart
@@ -85,13 +85,15 @@ class MultipartRequest extends BaseRequest {
   /// Freezes all mutable fields and returns a single-subscription [ByteStream]
   /// that will emit the request body.
   @override
-  ByteStream finalize() => ByteStream(_finalize());
-
-  Stream<List<int>> _finalize() async* {
-    // TODO(nweiz): freeze fields and files
-    var boundary = _boundaryString();
+  ByteStream finalize() {
+    // TODO: freeze fields and files
+    final boundary = _boundaryString();
     headers['content-type'] = 'multipart/form-data; boundary=$boundary';
     super.finalize();
+    return ByteStream(_finalize(boundary));
+  }
+
+  Stream<List<int>> _finalize(String boundary) async* {
     const line = [13, 10]; // \r\n
     final separator = utf8.encode('--$boundary\r\n');
     final close = utf8.encode('--$boundary--\r\n');
