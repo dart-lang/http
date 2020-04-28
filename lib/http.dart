ebd4d174e7d023d6da9a35f653cfb1584168b8db
Lucas Tomasi <lcstomasi@gmail.com>
2020-04-28 14:26:21 -0300

    Adding possibility of send body into a request using get method

--- lib/http.dart
+++ lib/http.dart
@@ -37,13 +37,25 @@ Future<Response> head(url, {Map<String, String> headers}) =>
 /// Sends an HTTP GET request with the given headers to the given URL, which can
 /// be a [Uri] or a [String].
 ///
+/// [body] sets the body of the request. It can be a [String], a [List<int>]
+/// or a [Map<String, String>]. If it's a String, it's encoded using
+/// [encoding] and used as the body of the request. The content-type of the
+/// request will default to "text/plain".
+///
+/// If [body] is a List, it's used as a list of bytes for the body of the
+/// request.
+///
+/// If [body] is a Map, it's encoded as form fields using [encoding]. The
+/// content-type of the request will be set to
+/// `"application/x-www-form-urlencoded"`; this cannot be overridden.
+///
 /// This automatically initializes a new [Client] and closes that client once
 /// the request is complete. If you're planning on making multiple requests to
 /// the same server, you should use a single [Client] for all of those requests.
 ///
 /// For more fine-grained control over the request, use [Request] instead.
-Future<Response> get(url, {Map<String, String> headers}) =>
-    _withClient((client) => client.get(url, headers: headers));
+Future<Response> get(url, {Map<String, String> headers, body}) =>
+    _withClient((client) => client.get(url, headers: headers, body: body));
 
 /// Sends an HTTP POST request with the given headers and body to the given URL,
 /// which can be a [Uri] or a [String].
--- lib/src/base_client.dart
+++ lib/src/base_client.dart
@@ -29,10 +29,22 @@ abstract class BaseClient implements Client {
   /// Sends an HTTP GET request with the given headers to the given URL, which
   /// can be a [Uri] or a [String].
   ///
+  /// [body] sets the body of the request. It can be a [String], a [List<int>]
+  /// or a [Map<String, String>]. If it's a String, it's encoded using
+  /// [encoding] and used as the body of the request. The content-type of the
+  /// request will default to "text/plain".
+  ///
+  /// If [body] is a List, it's used as a list of bytes for the body of the
+  /// request.
+  ///
+  /// If [body] is a Map, it's encoded as form fields using [encoding]. The
+  /// content-type of the request will be set to
+  /// `"application/x-www-form-urlencoded"`; this cannot be overridden.
+  ///
   /// For more fine-grained control over the request, use [send] instead.
   @override
-  Future<Response> get(url, {Map<String, String> headers}) =>
-      _sendUnstreamed('GET', url, headers);
+  Future<Response> get(url, {Map<String, String> headers, body}) =>
+      _sendUnstreamed('GET', url, headers, body);
 
   /// Sends an HTTP POST request with the given headers and body to the given
   /// URL, which can be a [Uri] or a [String].
