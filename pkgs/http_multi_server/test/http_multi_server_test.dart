264d705391191777f323499e3d8c24dc89479553
Gary Roumanis <grouma@google.com>
2021-04-12 12:52:37 -0700

    Bind to the correct address for

--- pkgs/http_multi_server/test/http_multi_server_test.dart
+++ pkgs/http_multi_server/test/http_multi_server_test.dart
@@ -219,6 +219,18 @@ void main() {
       }
     });
 
+    test("uses the correct server address for 'any'", () async {
+      final server = await HttpMultiServer.bind('any', 0);
+
+      if (await supportsIPv4) {
+        expect(server.address, InternetAddress.anyIPv4);
+      }
+
+      if (await supportsIPv6) {
+        expect(server.address, InternetAddress.anyIPv6);
+      }
+    });
+
     test('listens on specified hostname', () async {
       if (!await supportsIPv4) return;
       final server = await HttpMultiServer.bind(InternetAddress.anyIPv4, 0);
