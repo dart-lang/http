97ca5e974814748c79967b934cccff93d8c11df8
Kevin Moore <kevmoo@users.noreply.github.com>
2021-04-23 10:54:25 -0700

    enable avoid dynamic calls lint (#560)
    Co-authored-by: Nate Bosch <nbosch@google.com>

--- example/main.dart
+++ example/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert' as convert;
+
 import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
@@ -10,7 +11,8 @@ void main(List<String> arguments) async {
   // Await the http get response, then decode the json-formatted response.
   var response = await http.get(url);
   if (response.statusCode == 200) {
-    var jsonResponse = convert.jsonDecode(response.body);
+    var jsonResponse =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
     var itemCount = jsonResponse['totalItems'];
     print('Number of books about http: $itemCount.');
   } else {
