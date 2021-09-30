e3678c0a7fc55a5c81b7f9ae82da0706a56db3fe
Khaled ElMorshedy <elmoiv@yahoo.com>
2021-09-30 15:16:18 +0200

    Update browser_client.dart

--- lib/src/browser_client.dart
+++ lib/src/browser_client.dart
@@ -59,6 +59,7 @@ class BrowserClient extends BaseClient {
           contentLength: body.length,
           request: request,
           headers: xhr.responseHeaders,
+          url: xhr.responseUrl,
           reasonPhrase: xhr.statusText));
     }));
 
