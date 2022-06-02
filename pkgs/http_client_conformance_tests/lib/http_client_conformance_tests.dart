c09fb1d4fa0859f79cab46dacf5f430ab46476dd
Brian Quinlan <bquinlan@google.com>
2022-06-02 15:10:35 -0700

    Run the redirect tests against the browser client. (#708)

--- pkgs/http_client_conformance_tests/lib/http_client_conformance_tests.dart
+++ pkgs/http_client_conformance_tests/lib/http_client_conformance_tests.dart
@@ -25,11 +25,16 @@ export 'src/response_headers_tests.dart' show testResponseHeaders;
 /// If [canStreamResponseBody] is `false` then tests that assume that the
 /// [Client] supports receiving HTTP responses with unbounded body sizes will
 /// be skipped
+///
+/// If [redirectAlwaysAllowed] is `true` then tests that require the [Client]
+/// to limit redirects will be skipped.
 void testAll(Client client,
-    {bool canStreamRequestBody = true, bool canStreamResponseBody = true}) {
+    {bool canStreamRequestBody = true,
+    bool canStreamResponseBody = true,
+    bool redirectAlwaysAllowed = false}) {
   testRequestBody(client, canStreamRequestBody: canStreamRequestBody);
   testResponseBody(client, canStreamResponseBody: canStreamResponseBody);
   testRequestHeaders(client);
   testResponseHeaders(client);
-  testRedirect(client);
+  testRedirect(client, redirectAlwaysAllowed: redirectAlwaysAllowed);
 }
--- pkgs/http_client_conformance_tests/test/browser_client_test.dart
+++ pkgs/http_client_conformance_tests/test/browser_client_test.dart
@@ -2,16 +2,16 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@TestOn('!js')
+@TestOn('browser')
 
-import 'package:http/io_client.dart';
+import 'package:http/browser_client.dart';
 import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final client = IOClient();
+  final client = BrowserClient();
 
   group('testAll', () {
-    testAll(client);
+    testRedirect(client, redirectAlwaysAllowed: true);
   });
 }
