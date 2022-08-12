4b21c027fbba230d2e69c032c32a7f4f301f2821
Brian Quinlan <bquinlan@google.com>
2022-08-12 14:58:21 -0700

    Remove testWidgets

--- pkgs/cronet_http/example/integration_test/client_conformance_test.dart
+++ pkgs/cronet_http/example/integration_test/client_conformance_test.dart
@@ -3,18 +3,11 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:cronet_http/cronet_client.dart';
-import 'package:flutter_test/flutter_test.dart';
-// import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
+import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('MyWidget', (WidgetTester tester) async {
-    await CronetClient().get(Uri.parse('https://www.example.com'));
-
-//    expect(find.text('Success'), findsOneWidget);
-  });
-
-//  testAll(CronetClient(), canStreamRequestBody: false);
+  testAll(CronetClient(), canStreamRequestBody: false);
 }
