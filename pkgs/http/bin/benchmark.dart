a39b0bef6e3fc10fd84eda1854e08e9975fb651e
Brian Quinlan <bquinlan@google.com>
2022-08-17 14:20:52 -0700

    Remove benchmarking stuff

--- pkgs/http/bin/benchmark.dart
+++
@@ -1,6 +0,0 @@
-import 'package:http/io_client.dart';
-import 'package:http_client_conformance_tests/http_client_benchmarks.dart';
-
-void main() {
-  benchmarkAll(() => IOClient()).listen(print);
-}
