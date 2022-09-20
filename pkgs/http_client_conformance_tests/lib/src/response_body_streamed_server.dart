462bbe7d049134e2cf1724ad2c9617e0feb93c2f
Brian Quinlan <bquinlan@google.com>
2022-09-20 14:33:20 -0700

    Fix foo

--- pkgs/http_client_conformance_tests/lib/src/response_body_streamed_server.dart
+++ pkgs/http_client_conformance_tests/lib/src/response_body_streamed_server.dart
@@ -26,10 +26,6 @@ void hybridMain(StreamChannel<Object?> channel) async {
       await request.drain<void>();
       request.response.headers.set('Access-Control-Allow-Origin', '*');
       request.response.headers.set('Content-Type', 'text/plain');
-      if (request.requestedUri.pathSegments.isNotEmpty &&
-          request.requestedUri.pathSegments.last == 'length') {
-        request.response.contentLength = 1000;
-      }
       serverWriting = true;
       for (var i = 0; serverWriting; ++i) {
         request.response.write('$i\n');
