98ab7627d181de25d61cca95f5eaddc5ba3d5a2f
Brian Quinlan <bquinlan@google.com>
2022-12-14 15:01:54 -0800

    Add test

---
+++ pkgs/cupertino_http/example/integration_test/example_test.dart
@@ -0,0 +1,26 @@
+@Skip('currently failing (see issue 1234)')
+
+import 'package:cupertino_http_example/main.dart' as app;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('search test', () {
+    testWidgets('search for a book', (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      final search = find.byType(TextField);
+      await tester.enterText(search, 'Dart Apprentice (First Edition)');
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+
+      expect(
+          // From the synopsis for 'Dart Apprentice (First Edition).
+          find.textContaining('Make Dart Your Programming Language of Choice'),
+          findsOneWidget);
+    });
+  });
+}
