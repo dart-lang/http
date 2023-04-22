cb7e7e7da674f6edd1f53f5a055396878c4b5ab2
Seif Ashraf <seifibrahim32@gmail.com>
2023-04-22 23:53:16 +0200

    Showed some examples on using HttpHeaders class.

--- pkgs/http/example/main.dart
+++ pkgs/http/example/main.dart
@@ -1,12 +1,19 @@
 import 'dart:convert' as convert;
 
 import 'package:http/http.dart' as http;
+import 'package:http/http_headers_utils/http_headers.dart';
 
 void main(List<String> arguments) async {
   // This example uses the Google Books API to search for books about http.
   // https://developers.google.com/books/docs/overview
   var url =
       Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+
+  // Fetching data from the Uri using HttpHeaders' ContentType's class,
+  // Navigate to lib/http_headers_utils to know more..
+  await http.get(url,headers: {
+    HttpHeaders.contentTypeHeader : ContentType.json.primaryType
+  }).then((response) => print(response.body));
 
   // Await the http get response, then decode the json-formatted response.
   var response = await http.get(url);
