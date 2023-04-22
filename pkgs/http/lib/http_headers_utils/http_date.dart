2b9cfe35811aa2d361aa3f63ac5435c773cc1652
Seif Ashraf <seifibrahim32@gmail.com>
2023-04-23 00:13:17 +0200

    Added Flutter Lints

--- pkgs/http/lib/http_headers_utils/http_date.dart
+++ pkgs/http/lib/http_headers_utils/http_date.dart
@@ -2,7 +2,7 @@ import '../src/exception.dart';
 
 /// Utility functions for working with dates with HTTP specific date
 /// formats.
-///
+// ignore_for_file: avoid_classes_with_only_static_members
 
 abstract class HttpDate {
   // From RFC-2616 section "3.3.1 Full Date",
@@ -32,39 +32,39 @@ abstract class HttpDate {
   /// [RFC-1123](http://tools.ietf.org/html/rfc1123 "RFC-1123"),
   /// e.g. `Thu, 1 Jan 1970 00:00:00 GMT`.
   static String format(DateTime date) {
-    const List wkday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    const List month = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
+    const wkday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const month = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
     ];
 
-    DateTime d = date.toUtc();
-    StringBuffer sb = StringBuffer()
+    var d = date.toUtc();
+    var sb = StringBuffer()
       ..write(wkday[d.weekday - 1])
-      ..write(", ")
-      ..write(d.day <= 9 ? "0" : "")
+      ..write(', ')
+      ..write(d.day <= 9 ? '0' : '')
       ..write(d.day.toString())
-      ..write(" ")
+      ..write(' ')
       ..write(month[d.month - 1])
-      ..write(" ")
+      ..write(' ')
       ..write(d.year.toString())
-      ..write(d.hour <= 9 ? " 0" : " ")
+      ..write(d.hour <= 9 ? ' 0' : ' ')
       ..write(d.hour.toString())
-      ..write(d.minute <= 9 ? ":0" : ":")
+      ..write(d.minute <= 9 ? ':0' : ':')
       ..write(d.minute.toString())
-      ..write(d.second <= 9 ? ":0" : ":")
+      ..write(d.second <= 9 ? ':0' : ':')
       ..write(d.second.toString())
-      ..write(" GMT");
+      ..write(' GMT');
     return sb.toString();
   }
 
@@ -77,50 +77,49 @@ abstract class HttpDate {
   ///     Thursday, 1-Jan-1970 00:00:00 GMT
   ///     Thu Jan  1 00:00:00 1970
   ///
-  /// For more information see [RFC-2616 section
-  /// 3.1.1](http://tools.ietf.org/html/rfc2616#section-3.3.1
-  /// "RFC-2616 section 3.1.1").
+  /// For more information see [RFC-2616 section 3.1.1](http://tools.ietf.org/h
+  /// tml/rfc2616#section-3.3.1"RFC-2616 section 3.1.1").
   static DateTime parse(String date) {
-    final int SP = 32;
-    const List wkdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    const List weekdays = [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday"
+    var SP = 32;
+    const wkdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const weekdays = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday'
     ];
-    const List months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
     ];
 
-    final int formatRfc1123 = 0;
-    final int formatRfc850 = 1;
-    final int formatAsctime = 2;
+    const formatRfc1123 = 0;
+    const formatRfc850 = 1;
+    const formatAsctime = 2;
 
-    int index = 0;
+    var index = 0;
     String tmp;
 
     void expect(String s) {
       if (date.length - index < s.length) {
-        throw HttpException("Invalid HTTP date $date");
+        throw HttpException('Invalid HTTP date $date');
       }
-      String tmp = date.substring(index, index + s.length);
+      var tmp = date.substring(index, index + s.length);
       if (tmp != s) {
-        throw HttpException("Invalid HTTP date $date");
+        throw HttpException('Invalid HTTP date $date');
       }
       index += s.length;
     }
@@ -128,10 +127,10 @@ abstract class HttpDate {
     int expectWeekday() {
       int weekday;
       // The formatting of the weekday signals the format of the date string.
-      int pos = date.indexOf(",", index);
+      var pos = date.indexOf(',', index);
       if (pos == -1) {
-        int pos = date.indexOf(" ", index);
-        if (pos == -1) throw HttpException("Invalid HTTP date $date");
+        var pos = date.indexOf(' ', index);
+        if (pos == -1) throw HttpException('Invalid HTTP date $date');
         tmp = date.substring(index, pos);
         index = pos + 1;
         weekday = wkdays.indexOf(tmp);
@@ -150,17 +149,17 @@ abstract class HttpDate {
           return formatRfc850;
         }
       }
-      throw HttpException("Invalid HTTP date $date");
+      throw HttpException('Invalid HTTP date $date');
     }
 
     int expectMonth(String separator) {
-      int pos = date.indexOf(separator, index);
-      if (pos - index != 3) throw HttpException("Invalid HTTP date $date");
+      var pos = date.indexOf(separator, index);
+      if (pos - index != 3) throw HttpException('Invalid HTTP date $date');
       tmp = date.substring(index, pos);
       index = pos + 1;
-      int month = months.indexOf(tmp);
+      var month = months.indexOf(tmp);
       if (month != -1) return month;
-      throw HttpException("Invalid HTTP date $date");
+      throw HttpException('Invalid HTTP date $date');
     }
 
     int expectNum(String separator) {
@@ -170,23 +169,23 @@ abstract class HttpDate {
       } else {
         pos = date.length;
       }
-      String tmp = date.substring(index, pos);
+      var tmp = date.substring(index, pos);
       index = pos + separator.length;
       try {
-        int value = int.parse(tmp);
+        var value = int.parse(tmp);
         return value;
       } on FormatException {
-        throw HttpException("Invalid HTTP date $date");
+        throw HttpException('Invalid HTTP date $date');
       }
     }
 
     void expectEnd() {
       if (index != date.length) {
-        throw HttpException("Invalid HTTP date $date");
+        throw HttpException('Invalid HTTP date $date');
       }
     }
 
-    int format = expectWeekday();
+    var format = expectWeekday();
     int year;
     int month;
     int day;
@@ -194,22 +193,22 @@ abstract class HttpDate {
     int minutes;
     int seconds;
     if (format == formatAsctime) {
-      month = expectMonth(" ");
+      month = expectMonth(' ');
       if (date.codeUnitAt(index) == SP) index++;
-      day = expectNum(" ");
-      hours = expectNum(":");
-      minutes = expectNum(":");
-      seconds = expectNum(" ");
-      year = expectNum("");
+      day = expectNum(' ');
+      hours = expectNum(':');
+      minutes = expectNum(':');
+      seconds = expectNum(' ');
+      year = expectNum('');
     } else {
-      expect(" ");
-      day = expectNum(format == formatRfc1123 ? " " : "-");
-      month = expectMonth(format == formatRfc1123 ? " " : "-");
-      year = expectNum(" ");
-      hours = expectNum(":");
-      minutes = expectNum(":");
-      seconds = expectNum(" ");
-      expect("GMT");
+      expect(' ');
+      day = expectNum(format == formatRfc1123 ? ' ' : '-');
+      month = expectMonth(format == formatRfc1123 ? ' ' : '-');
+      year = expectNum(' ');
+      hours = expectNum(':');
+      minutes = expectNum(':');
+      seconds = expectNum(' ');
+      expect('GMT');
     }
     expectEnd();
     return DateTime.utc(year, month + 1, day, hours, minutes, seconds, 0);
@@ -217,31 +216,31 @@ abstract class HttpDate {
 
   // Parse a cookie date string.
   static DateTime parseCookieDate(String date) {
-    const List monthsLowerCase = [
-      "jan",
-      "feb",
-      "mar",
-      "apr",
-      "may",
-      "jun",
-      "jul",
-      "aug",
-      "sep",
-      "oct",
-      "nov",
-      "dec"
+    const monthsLowerCase = [
+      'jan',
+      'feb',
+      'mar',
+      'apr',
+      'may',
+      'jun',
+      'jul',
+      'aug',
+      'sep',
+      'oct',
+      'nov',
+      'dec'
     ];
 
-    int position = 0;
+    var position = 0;
 
     Never error() {
-      throw HttpException("Invalid cookie date $date");
+      throw HttpException('Invalid cookie date $date');
     }
 
     bool isEnd() => position == date.length;
 
     bool isDelimiter(String s) {
-      int char = s.codeUnitAt(0);
+      var char = s.codeUnitAt(0);
       if (char == 0x09) return true;
       if (char >= 0x20 && char <= 0x2F) return true;
       if (char >= 0x3B && char <= 0x40) return true;
@@ -251,7 +250,7 @@ abstract class HttpDate {
     }
 
     bool isNonDelimiter(String s) {
-      int char = s.codeUnitAt(0);
+      var char = s.codeUnitAt(0);
       if (char >= 0x00 && char <= 0x08) return true;
       if (char >= 0x0A && char <= 0x1F) return true;
       if (char >= 0x30 && char <= 0x39) return true; // Digit
@@ -263,7 +262,7 @@ abstract class HttpDate {
     }
 
     bool isDigit(String s) {
-      int char = s.codeUnitAt(0);
+      var char = s.codeUnitAt(0);
       if (char > 0x2F && char < 0x3A) return true;
       return false;
     }
@@ -274,18 +273,26 @@ abstract class HttpDate {
     }
 
     int toInt(String s) {
-      int index = 0;
-      for (; index < s.length && isDigit(s[index]); index++);
+      var index = 0;
+      for (; index < s.length && isDigit(s[index]); ) {
+        index++;
+      }
       return int.parse(s.substring(0, index));
     }
 
     var tokens = <String>[];
     while (!isEnd()) {
-      while (!isEnd() && isDelimiter(date[position])) position++;
-      int start = position;
-      while (!isEnd() && isNonDelimiter(date[position])) position++;
+      while (!isEnd() && isDelimiter(date[position])) {
+        position++;
+      }
+      var start = position;
+      while (!isEnd() && isNonDelimiter(date[position])) {
+        position++;
+      }
       tokens.add(date.substring(start, position).toLowerCase());
-      while (!isEnd() && isDelimiter(date[position])) position++;
+      while (!isEnd() && isDelimiter(date[position])) {
+        position++;
+      }
     }
 
     String? timeStr;
@@ -298,7 +305,7 @@ abstract class HttpDate {
       if (timeStr == null &&
           token.length >= 5 &&
           isDigit(token[0]) &&
-          (token[1] == ":" || (isDigit(token[1]) && token[2] == ":"))) {
+          (token[1] == ':' || (isDigit(token[1]) && token[2] == ':'))) {
         timeStr = token;
       } else if (dayOfMonthStr == null && isDigit(token[0])) {
         dayOfMonthStr = token;
@@ -319,22 +326,24 @@ abstract class HttpDate {
       error();
     }
 
-    int year = toInt(yearStr);
-    if (year >= 70 && year <= 99)
+    var year = toInt(yearStr);
+    if (year >= 70 && year <= 99) {
       year += 1900;
-    else if (year >= 0 && year <= 69) year += 2000;
+    } else if (year >= 0 && year <= 69) {
+      year += 2000;
+    }
     if (year < 1601) error();
 
-    int dayOfMonth = toInt(dayOfMonthStr);
+    var dayOfMonth = toInt(dayOfMonthStr);
     if (dayOfMonth < 1 || dayOfMonth > 31) error();
 
-    int month = getMonth(monthStr) + 1;
+    var month = getMonth(monthStr) + 1;
 
-    var timeList = timeStr.split(":");
+    var timeList = timeStr.split(':');
     if (timeList.length != 3) error();
-    int hour = toInt(timeList[0]);
-    int minute = toInt(timeList[1]);
-    int second = toInt(timeList[2]);
+    var hour = toInt(timeList[0]);
+    var minute = toInt(timeList[1]);
+    var second = toInt(timeList[2]);
     if (hour > 23) error();
     if (minute > 59) error();
     if (second > 59) error();
