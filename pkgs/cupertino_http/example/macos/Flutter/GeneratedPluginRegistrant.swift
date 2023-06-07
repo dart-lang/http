88f5e5fecbceca83ce5466fdb8d74f1e9a39664d
Brian Quinlan <bquinlan@google.com>
2023-06-07 15:03:06 -0700

    Revery data files

--- pkgs/cupertino_http/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pkgs/cupertino_http/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_foundation
-import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
