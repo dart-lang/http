bd8e3f6d296cda13063c1d965ab8c7bac5f0160d
Alex James <alexander.james.developer@gmail.com>
2023-06-21 19:36:31 +0100

    Create java_http package
    Create a new Dart package in pkgs called java_http using the Dart create
    command and the package template.

---
+++ pkgs/java_http/example/java_http_example.dart
@@ -0,0 +1,6 @@
+import 'package:java_http/java_http.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ pkgs/java_http/test/java_http_test.dart
@@ -0,0 +1,16 @@
+import 'package:java_http/java_http.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
