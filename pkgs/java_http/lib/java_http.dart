3d2b0d62a421ddd0318deb461ed136da41d614c2
Alex James <alexander.james.developer@gmail.com>
2023-06-23 15:33:22 +0100

    Remove comments generated by Dart create

--- pkgs/java_http/lib/java_http.dart
+++ pkgs/java_http/lib/java_http.dart
@@ -2,11 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
 export 'src/java_http_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- pkgs/java_http/lib/src/java_http_base.dart
+++ pkgs/java_http/lib/src/java_http_base.dart
@@ -2,9 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
 }
