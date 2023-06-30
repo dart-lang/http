d5009564abb24f23199f4fa5c043824a748a47fd
Brian Quinlan <bquinlan@google.com>
2023-06-30 14:36:12 -0700

    Add a note pointing out that a network entitlement is required to use cupertino_http

--- pkgs/cupertino_http/lib/cupertino_http.dart
+++ pkgs/cupertino_http/lib/cupertino_http.dart
@@ -5,6 +5,12 @@
 /// Provides access to the
 /// [Foundation URL Loading System](https://developer.apple.com/documentation/foundation/url_loading_system).
 ///
+/// **NOTE**: The
+/// [`com.apple.security.network.client`](https://developer.apple.com/documentation/bundleresources/entitlements/com_apple_security_network_client)
+/// entitlement is required to use `package:cupertino_http` on iOS. Check out
+/// the
+/// [example app configuration](https://github.com/dart-lang/http/blob/d68081f50f0f0a8928e42237e937dcc69cc5d890/pkgs/cupertino_http/example/ios/Flutter/AppFrameworkInfo.plist).
+/// 
 /// # CupertinoClient
 ///
 /// The most convenient way to `package:cupertino_http` it is through
