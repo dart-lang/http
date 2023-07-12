7f618843ca833eefbc7c4a49965597afe7b57fa9
Alex James <alexander.james.developer@gmail.com>
2023-07-12 16:46:19 +0100

    Dart nalyze warning

--- pkgs/java_http/test/java_client_test.dart
+++ pkgs/java_http/test/java_client_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
+//import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
 import 'package:java_http/java_http.dart';
 import 'package:test/test.dart';
 
