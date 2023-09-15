5848fc3dad2df651c4430ee5e3ec47169ecf23e9
Brian Quinlan <bquinlan@google.com>
2023-09-14 17:16:55 -0700

    Fix it up

--- pkgs/http_client_conformance_tests/lib/src/response_status_line_tests.dart
+++ pkgs/http_client_conformance_tests/lib/src/response_status_line_tests.dart
@@ -10,7 +10,8 @@ import 'package:test/test.dart';
 import 'response_status_line_server_vm.dart'
     if (dart.library.html) 'response_status_line_server_web.dart';
 
-/// Tests that the [Client] correctly processes the response status line.
+/// Tests that the [Client] correctly processes the response status line (e.g.
+/// 'HTTP/1.1 200 OK\r\n').
 ///
 /// If [reasonableInvalidStatusLineHandling] is `false` the tests related to
 /// invalid `Status-Line`s are skipped.
@@ -27,44 +28,53 @@ void testResponseStatusLine(Client client,
       host = 'localhost:${await httpServerQueue.next}';
     });
 
-    test(
-      'without HTTP version',
-      () async {
-        httpServerChannel.sink.add('201 Created');
-        await expectLater(
-          client.get(Uri.http(host, '')),
-          throwsA(isA<ClientException>()),
-        );
-      },
-    );
+    test('valid', () async {
+      httpServerChannel.sink.add('HTTP/1.1 201 Created');
+      final response = await client.get(Uri.http(host, '');
+      expect(response.statusCode, 201);
+      expect(response.reasonPhrase, 'Created');
+    });
 
-    test(
-      'without status code',
-      () async {
-        httpServerChannel.sink.add('HTTP/1.1 OK');
-        await expectLater(
-          client.get(Uri.http(host, '')),
-          throwsA(isA<ClientException>()),
-        );
-      },
-    );
+    group('invalid', () {
+      test(
+        'without HTTP version',
+        () async {
+          httpServerChannel.sink.add('201 Created');
+          await expectLater(
+            client.get(Uri.http(host, '')),
+            throwsA(isA<ClientException>()),
+          );
+        },
+      );
 
-    test(
-      'without reason phrase',
-      () async {
-        httpServerChannel.sink.add('HTTP/1.1 201');
-        try {
-          final response = await client.get(Uri.http(host, ''));
-          expect(response.statusCode, 201);
-          // All of these responses seem reasonable.
-          expect(response.reasonPhrase, anyOf(isNull, '', 'Created'));
-        } on ClientException {
-          // A Reason-Phrase is required according to RFC-2616
-        }
-      },
-    );
-  },
-      skip: reasonableInvalidStatusLineHandling
-          ? false
-          : 'does handle invalid request lines in a sensible way');
+      test(
+        'without status code',
+        () async {
+          httpServerChannel.sink.add('HTTP/1.1 OK');
+          await expectLater(
+            client.get(Uri.http(host, '')),
+            throwsA(isA<ClientException>()),
+          );
+        },
+      );
+
+      test(
+        'without reason phrase',
+        () async {
+          httpServerChannel.sink.add('HTTP/1.1 201');
+          try {
+            final response = await client.get(Uri.http(host, ''));
+            expect(response.statusCode, 201);
+            // All of these responses seem reasonable.
+            expect(response.reasonPhrase, anyOf(isNull, '', 'Created'));
+          } on ClientException {
+            // A Reason-Phrase is required according to RFC-2616
+          }
+        },
+      );
+    },
+        skip: reasonableInvalidStatusLineHandling
+            ? false
+            : 'does handle invalid request lines in a sensible way');
+  });
 }
