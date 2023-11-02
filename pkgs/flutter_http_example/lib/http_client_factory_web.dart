600e119a72b1b9968e1e4d5904efaa18d33e0ce7
Brian Quinlan <bquinlan@google.com>
2023-11-02 08:16:53 -0700

    Web sort order

--- pkgs/flutter_http_example/lib/http_client_factory_web.dart
+++ pkgs/flutter_http_example/lib/http_client_factory_web.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:http/http.dart';
 import 'package:fetch_client/fetch_client.dart';
+import 'package:http/http.dart';
 
 Client httpClient() => FetchClient(mode: RequestMode.cors);
