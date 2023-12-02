a2d0721c7120aad227f4933bba59d0d6b4f8f5ca
Kevin Moore <kevmoo@google.com>
2023-12-01 17:23:38 -0800

    comment

--- pkgs/http_client_conformance_tests/lib/src/server_queue_helpers.dart
+++ pkgs/http_client_conformance_tests/lib/src/server_queue_helpers.dart
@@ -5,5 +5,6 @@
 import 'package:async/async.dart';
 
 extension StreamQueueOfNullableObjectExtension on StreamQueue<Object?> {
+  /// When run under dart2wasm, JSON numbers are always returned as [double].
   Future<int> get nextAsInt async => ((await next) as num).toInt();
 }
