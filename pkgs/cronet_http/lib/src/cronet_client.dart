6873731c7bdf14e2489587de66ad1a115b35747f
Brian Quinlan <brian@sweetapp.com>
2024-02-22 09:08:28 -0800

    Make it possible for `CronetClient` to own the lifetime of an existing `CronetEngine` (#1137)

--- pkgs/cronet_http/lib/src/cronet_client.dart
+++ pkgs/cronet_http/lib/src/cronet_client.dart
@@ -49,6 +49,7 @@ enum CacheMode {
 /// An environment that can be used to make HTTP requests.
 class CronetEngine {
   late final jb.CronetEngine _engine;
+  bool _isClosed = false;
 
   CronetEngine._(this._engine);
 
@@ -140,7 +141,12 @@ class CronetEngine {
   }
 
   void close() {
-    _engine.shutdown();
+    if (!_isClosed) {
+      _engine
+        ..shutdown()
+        ..release();
+    }
+    _isClosed = true;
   }
 }
 
@@ -275,12 +281,10 @@ class CronetClient extends BaseClient {
   CronetEngine? _engine;
   bool _isClosed = false;
 
-  /// Indicates that [_engine] was constructed as an implementation detail for
-  /// this [CronetClient] (i.e. was not provided as a constructor argument) and
-  /// should be closed when this [CronetClient] is closed.
-  final bool _ownedEngine;
+  /// Indicates that [CronetClient] is responsible for closing [_engine].
+  final bool _closeEngine;
 
-  CronetClient._(this._engine, this._ownedEngine) {
+  CronetClient._(this._engine, this._closeEngine) {
     Jni.initDLApi();
   }
 
@@ -288,8 +292,13 @@ class CronetClient extends BaseClient {
   factory CronetClient.defaultCronetEngine() => CronetClient._(null, true);
 
   /// A [CronetClient] configured with a [CronetEngine].
-  factory CronetClient.fromCronetEngine(CronetEngine engine) =>
-      CronetClient._(engine, false);
+  ///
+  /// If [closeEngine] is `true`, then [engine] will be closed when [close] is
+  /// called on this [CronetClient]. This can simplify lifetime management if
+  /// [engine] is only used in one [CronetClient].
+  factory CronetClient.fromCronetEngine(CronetEngine engine,
+          {bool closeEngine = false}) =>
+      CronetClient._(engine, closeEngine);
 
   /// A [CronetClient] configured with a [Future] containing a [CronetEngine].
   ///
@@ -309,7 +318,7 @@ class CronetClient extends BaseClient {
   /// ```
   @override
   void close() {
-    if (!_isClosed && _ownedEngine) {
+    if (!_isClosed && _closeEngine) {
       _engine?.close();
     }
     _isClosed = true;
@@ -322,14 +331,19 @@ class CronetClient extends BaseClient {
           'HTTP request failed. Client is already closed.', request.url);
     }
 
-    _engine ??= CronetEngine.build();
+    final engine = _engine ?? CronetEngine.build();
+    _engine = engine;
+
+    if (engine._isClosed) {
+      throw ClientException(
+          'HTTP request failed. CronetEngine is already closed.', request.url);
+    }
 
     final stream = request.finalize();
     final body = await stream.toBytes();
     final responseCompleter = Completer<StreamedResponse>();
-    final engine = _engine!._engine;
 
-    final builder = engine.newUrlRequestBuilder(
+    final builder = engine._engine.newUrlRequestBuilder(
       request.url.toString().toJString(),
       jb.UrlRequestCallbackProxy.new1(
           _urlRequestCallbacks(request, responseCompleter)),
