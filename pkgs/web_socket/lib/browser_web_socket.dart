557c420028e759400b6b0f4e1cd17b3eca1c4c6d
Brian Quinlan <brian@sweetapp.com>
2024-03-04 11:25:31 -0800

    Implement WebSocket for the browser (#1142)

--- pkgs/web_socket/lib/browser_web_socket.dart
+++ pkgs/web_socket/lib/browser_web_socket.dart
@@ -2,4 +2,4 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/web_socket.dart';
+export 'src/browser_web_socket.dart' show BrowserWebSocket;
