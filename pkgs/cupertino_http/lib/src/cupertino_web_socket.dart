1aa6aa882ce301365361b09de2cf970a2f2252eb
Brian Quinlan <bquinlan@google.com>
2024-03-19 13:48:06 -0700

    Remove print

--- pkgs/cupertino_http/lib/src/cupertino_web_socket.dart
+++ pkgs/cupertino_http/lib/src/cupertino_web_socket.dart
@@ -189,7 +189,6 @@ class CupertinoWebSocket implements WebSocket {
       unawaited(_events.close());
       if (code != null) {
         reason = reason ?? '';
-        print('Closing with code: $code $reason');
         _task.cancelWithCloseCode(code, Data.fromList(utf8.encode(reason)));
       } else {
         _task.cancel();
