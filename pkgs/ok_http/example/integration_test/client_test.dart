df99b97651c760a6745c839aa6a5dc03dd1f7d4f
Anikate-De <anikate1de@gmail.com>
2024-05-29 10:24:17 +0530

    add integration tests to pkgs/ok_http/example/

---
+++ pkgs/ok_http/example/integration_test/client_test.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:ok_http/ok_http.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  await testConformance();
+}
+
+Future<void> testConformance() async {
+  group('ok_http client', () {
+    testAll(
+      OkHttpClient.new,
+      canStreamRequestBody: false,
+      canStreamResponseBody: false,
+      preservesMethodCase: true,
+      canSendCookieHeaders: true,
+      canReceiveSetCookieHeaders: true,
+    );
+  });
+}
