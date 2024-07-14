4efbfadf2e9de7bbf7b7da2e3ccc3d6dad3eef2a
Anikate-De <anikate1de@gmail.com>
2024-07-14 18:14:02 +0530

    remove websocket example

--- pkgs/ok_http/example/lib/main.dart
+++ pkgs/ok_http/example/lib/main.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -12,7 +11,6 @@ import 'package:http/io_client.dart';
 import 'package:http_image_provider/http_image_provider.dart';
 import 'package:ok_http/ok_http.dart';
 import 'package:provider/provider.dart';
-import 'package:web_socket/web_socket.dart';
 
 import 'book.dart';
 
@@ -50,46 +48,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  final _labels = ['HTTP', 'WebSocket'];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title:
-                Text(_selectedIndex == 0 ? 'Book Search' : 'WebSocket Echo')),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: (value) => setState(() {
-            _selectedIndex = value;
-          }),
-          items: [
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.cloud_outlined),
-              activeIcon: const Icon(Icons.cloud),
-              label: _labels[0],
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.swap_vert_circle_outlined),
-              activeIcon: const Icon(Icons.swap_vert_circle),
-              label: _labels[1],
-            ),
-          ],
-        ),
-        body: _selectedIndex == 0 ? const BookSearch() : const WebSocketEcho());
-  }
-}
-
-/// Example to demonstrate HTTP Calls with [OkHttpClient].
-class BookSearch extends StatefulWidget {
-  const BookSearch({super.key});
-
-  @override
-  State<BookSearch> createState() => _BookSearchState();
-}
-
-class _BookSearchState extends State<BookSearch> {
   List<Book>? _books;
   String? _lastQuery;
   late Client _client;
@@ -141,21 +99,24 @@ class _BookSearchState extends State<BookSearch> {
             ? BookList(_books!)
             : const Text('No results found', style: TextStyle(fontSize: 24));
 
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          TextField(
-            onChanged: _runSearch,
-            decoration: const InputDecoration(
-              labelText: 'Search',
-              suffixIcon: Icon(Icons.search),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Book Search')),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            TextField(
+              onChanged: _runSearch,
+              decoration: const InputDecoration(
+                labelText: 'Search',
+                suffixIcon: Icon(Icons.search),
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(child: searchResult),
-        ],
+            const SizedBox(height: 20),
+            Expanded(child: searchResult),
+          ],
+        ),
       ),
     );
   }
@@ -185,113 +146,4 @@ class _BookListState extends State<BookList> {
           ),
         ),
       );
-}
-
-/// Example to demonstrate WebSocket communication with [OkHttpWebSocket],
-/// using the echo server at `wss://echo.websocket.org`.
-class WebSocketEcho extends StatefulWidget {
-  const WebSocketEcho({super.key});
-
-  @override
-  State<WebSocketEcho> createState() => _WebSocketEchoState();
-}
-
-class _WebSocketEchoState extends State<WebSocketEcho> {
-  // Contains a list of pairs [String message, bool isSent].
-  final List<List> _messages = [];
-  final msgController = TextEditingController();
-
-  late WebSocket _webSocket;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                  child: ElevatedButton(
-                      onPressed: () async {
-                        try {
-                          _webSocket = await OkHttpWebSocket.connect(
-                              Uri.parse('wss://echo.websocket.org'));
-                        } on WebSocketException catch (e) {
-                          print('Error connecting to WebSocket: ${e.message}');
-                          return;
-                        }
-
-                        _webSocket.events.listen((event) {
-                          setState(() {
-                            switch (event) {
-                              case TextDataReceived(text: final text):
-                                _messages.add([text, false]);
-                                break;
-                              case CloseReceived():
-                                _messages.add(['Connection closed', false]);
-                              // For this example, binary data is not expected.
-                              default:
-                                _messages.add(
-                                    ['Unknown message received $event', false]);
-                            }
-                          });
-                        });
-                      },
-                      child: const Text('Connect'))),
-              const SizedBox(width: 16),
-              Expanded(
-                  child: ElevatedButton(
-                      onPressed: () {
-                        try {
-                          _webSocket.close();
-                        } on WebSocketException catch (e) {
-                          print('Error closing WebSocket: ${e.message}');
-                        }
-                      },
-                      child: const Text('Disconnect'))),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: msgController,
-                  decoration: const InputDecoration(
-                    labelText: 'Message',
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    _webSocket.sendText(msgController.text);
-                    setState(() {
-                      _messages.add([msgController.text, true]);
-                    });
-                  },
-                  child: const Text('Send')),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-              child: ListView.builder(
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_messages[index][0] as String),
-                trailing: Icon(_messages[index][1] as bool
-                    ? Icons.upload
-                    : Icons.download),
-              );
-            },
-            itemCount: _messages.length,
-          )),
-        ],
-      ),
-    );
-  }
 }
