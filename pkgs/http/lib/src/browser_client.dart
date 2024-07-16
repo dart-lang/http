d88fac5e5271ee4e5b04240a9290bafb086ee3e6
Kevin Moore <kevmoo@google.com>
2024-07-16 12:51:46 -0700

    Bump pkg:web dependency
    Use `show` on pkg:web so it's easier to see if usage changes
    Tighten constraints on dev dependencies

--- pkgs/http/lib/src/browser_client.dart
+++ pkgs/http/lib/src/browser_client.dart
@@ -5,7 +5,7 @@
 import 'dart:async';
 import 'dart:js_interop';
 
-import 'package:web/web.dart';
+import 'package:web/web.dart' show XMLHttpRequest, XHRGetters;
 
 import 'base_client.dart';
 import 'base_request.dart';
