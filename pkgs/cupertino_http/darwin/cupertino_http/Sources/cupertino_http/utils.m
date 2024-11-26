862fbde1801b3122f445c63290986ae300d9f6a9
Brian Quinlan <bquinlan@google.com>
2024-11-25 16:48:23 -0800

    Make symbol visible

--- pkgs/cupertino_http/darwin/cupertino_http/Sources/cupertino_http/utils.m
+++ pkgs/cupertino_http/darwin/cupertino_http/Sources/cupertino_http/utils.m
@@ -1,6 +1,21 @@
-#import "utils.h"
+#import <Foundation/NSLock.h>
+#import <Foundation/NSURL.h>
+#import <Foundation/NSURLSession.h>
+#include <stdint.h>
 
-_DidFinish adaptFinishWithLock(_DidFinishWithLock block) {
+#if !__has_feature(objc_arc)
+#error "This file must be compiled with ARC enabled"
+#endif
+
+typedef void (^_DidFinish)(void *closure, NSURLSession *session,
+                           NSURLSessionDownloadTask *downloadTask,
+                           NSURL *location);
+typedef void (^_DidFinishWithLock)(NSCondition *lock, NSURLSession *session,
+                                   NSURLSessionDownloadTask *downloadTask,
+                                   NSURL *location);
+
+__attribute__((visibility("default"))) __attribute__((used)) _DidFinish
+adaptFinishWithLock(_DidFinishWithLock block) {
   return ^void(void *closure, NSURLSession *session,
                NSURLSessionDownloadTask *downloadTask, NSURL *location) {
     NSCondition *lock = [[NSCondition alloc] init];
