d961c2f38af8c55d83b97207c3aa15982ba5ab3f
Brian Quinlan <bquinlan@google.com>
2024-11-25 16:56:33 -0800

    Fix headers

--- pkgs/cupertino_http/darwin/cupertino_http/Sources/cupertino_http/utils.m
+++ pkgs/cupertino_http/darwin/cupertino_http/Sources/cupertino_http/utils.m
@@ -1,21 +1,6 @@
-#import <Foundation/NSLock.h>
-#import <Foundation/NSURL.h>
-#import <Foundation/NSURLSession.h>
-#include <stdint.h>
+#include "utils.h"
 
-#if !__has_feature(objc_arc)
-#error "This file must be compiled with ARC enabled"
-#endif
-
-typedef void (^_DidFinish)(void *closure, NSURLSession *session,
-                           NSURLSessionDownloadTask *downloadTask,
-                           NSURL *location);
-typedef void (^_DidFinishWithLock)(NSCondition *lock, NSURLSession *session,
-                                   NSURLSessionDownloadTask *downloadTask,
-                                   NSURL *location);
-
-__attribute__((visibility("default"))) __attribute__((used)) _DidFinish
-adaptFinishWithLock(_DidFinishWithLock block) {
+_DidFinish adaptFinishWithLock(_DidFinishWithLock block) {
   return ^void(void *closure, NSURLSession *session,
                NSURLSessionDownloadTask *downloadTask, NSURL *location) {
     NSCondition *lock = [[NSCondition alloc] init];
