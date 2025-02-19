fd511c454650598e43eccb7ff55417c9121cf65f
seifibrahim32 <seifibrahim32@gmail.com>
2025-02-19 13:17:15 +0200

    Added mode WIP v.2

--- pkgs/http/lib/browser_client.dart
+++ pkgs/http/lib/browser_client.dart
@@ -2,4 +2,4 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/browser_client.dart' show BrowserClient, CacheMode;
+export 'src/browser_client.dart' show BrowserClient, CacheMode, RequestMode;
