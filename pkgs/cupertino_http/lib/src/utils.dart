18cdf52ed9fe64f18e59320a1cbbe2f9b5e7e88e
Brian Quinlan <bquinlan@google.com>
2025-03-11 15:25:43 -0700

    Unbreak

--- pkgs/cupertino_http/lib/src/utils.dart
+++ pkgs/cupertino_http/lib/src/utils.dart
@@ -2,25 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:ffi';
-import 'dart:io';
-
 import 'package:objective_c/objective_c.dart';
-
-import 'native_cupertino_bindings.dart' as ncb;
-
-/// Access to symbols that are linked into the process.
-///
-/// The "Foundation" framework is linked to Dart so no additional
-/// libraries need to be loaded to access those symbols.
-final ncb.NativeCupertinoHttp linkedLibs = () {
-  if (Platform.isMacOS || Platform.isIOS) {
-    final lib = DynamicLibrary.process();
-    return ncb.NativeCupertinoHttp(lib);
-  }
-  throw UnsupportedError(
-      'Platform ${Platform.operatingSystem} is not supported');
-}();
 
 /// Converts a NSDictionary containing NSString keys and NSString values into
 /// an equivalent map.
@@ -32,12 +14,12 @@ Map<String, String> stringNSDictionaryToMap(NSDictionary d) {
     if (!NSString.isInstance(nsKey)) {
       throw UnsupportedError('keys must be strings');
     }
-    final key = NSString.castFrom(nsKey).toString();
+    final key = NSString.castFrom(nsKey).toDartString();
     final nsValue = d.objectForKey_(nsKey);
     if (nsValue == null || !NSString.isInstance(nsValue)) {
       throw UnsupportedError('values must be strings');
     }
-    final value = NSString.castFrom(nsValue).toString();
+    final value = NSString.castFrom(nsValue).toDartString();
     m[key] = value;
   }
 
@@ -55,4 +37,4 @@ NSArray stringIterableToNSArray(Iterable<String> strings) {
 }
 
 NSURL uriToNSURL(Uri uri) => NSURL.URLWithString_(uri.toString().toNSString())!;
-Uri nsurlToUri(NSURL url) => Uri.parse(url.absoluteString!.toString());
+Uri nsurlToUri(NSURL url) => Uri.parse(url.absoluteString!.toDartString());
