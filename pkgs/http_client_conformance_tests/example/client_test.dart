dab425ea2eaf2c40517e4b221cce2cff7f1d116b
Brian Quinlan <bquinlan@google.com>
2025-03-21 11:54:01 -0700

    Adding missing license headers

--- pkgs/http_client_conformance_tests/example/client_test.dart
+++ pkgs/http_client_conformance_tests/example/client_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:http/http.dart';
 import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
 import 'package:test/test.dart';
