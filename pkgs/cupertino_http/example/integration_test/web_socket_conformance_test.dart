08ef2c3b6cda15226aafd9932c56fd7380f3ab2e
Brian Quinlan <brian@sweetapp.com>
2025-11-07 09:53:33 -0800

    fix(cupertino_http): Disable local close code tests on macOS (#1844)

--- pkgs/cupertino_http/example/integration_test/web_socket_conformance_test.dart
+++ pkgs/cupertino_http/example/integration_test/web_socket_conformance_test.dart
@@ -2,19 +2,39 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:cupertino_http/cupertino_http.dart';
 import 'package:test/test.dart';
+import 'package:web_socket/web_socket.dart';
 import 'package:web_socket_conformance_tests/web_socket_conformance_tests.dart';
 
-void main() {
-  testAll(CupertinoWebSocket.connect);
+void runTests(
+  Future<WebSocket> Function(Uri uri, {Iterable<String>? protocols})
+  webSocketFactory,
+) {
+  if (Platform.isMacOS) {
+    // TODO(https://github.com/dart-lang/http/issues/1814): Fix web socket tests
+    // on macOS.
+    testCloseRemote(webSocketFactory);
+    testConnectUri(webSocketFactory);
+    testDisconnectAfterUpgrade(webSocketFactory);
+    testNoUpgrade(webSocketFactory);
+    testPayloadTransfer(webSocketFactory);
+    testPeerProtocolErrors(webSocketFactory);
+    testProtocols(webSocketFactory);
+  } else {
+    testAll(webSocketFactory);
+  }
+}
 
+void main() {
   group('defaultSessionConfiguration', () {
-    testAll(CupertinoWebSocket.connect);
+    runTests(CupertinoWebSocket.connect);
   });
   group('fromSessionConfiguration', () {
     final config = URLSessionConfiguration.ephemeralSessionConfiguration();
-    testAll(
+    runTests(
       (uri, {protocols}) =>
           CupertinoWebSocket.connect(uri, protocols: protocols, config: config),
     );
