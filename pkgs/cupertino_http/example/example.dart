6e29dabcb5aaf3cdde67a5a46e89d253fd841c17
Brian Quinlan <bquinlan@google.com>
2026-03-23 17:54:28 -0700

    Update example.dart

--- pkgs/cupertino_http/example/example.dart
+++ pkgs/cupertino_http/example/example.dart
@@ -5,6 +5,10 @@ import 'package:cupertino_http/cupertino_http.dart';
 void main(List<String> arguments) async {
   // This example uses the Google Books API to search for books about http.
   // https://developers.google.com/books/docs/overview
+
+  // For a complete example of using `package:cupertino_http` in a Flutter
+  // application, see:
+  // https://github.com/dart-lang/http/tree/master/pkgs/flutter_http_example
   final client = CupertinoClient.defaultSessionConfiguration();
   final url = Uri.https('www.googleapis.com', '/books/v1/volumes', {
     'q': '{http}',
