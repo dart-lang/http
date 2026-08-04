09df9d9b77c490299d440603ed13adc4c37738f3
Seven Du <5564821+medz@users.noreply.github.com>
2026-08-04 13:29:52 +0800

    Fix RFC 850 comment lint

--- pkgs/http_parser/lib/src/http_date.dart
+++ pkgs/http_parser/lib/src/http_date.dart
@@ -71,8 +71,8 @@ DateTime parseHttpDate(String date) =>
         // RFC 9110, Section 5.6.7 says:
         // Recipients of a timestamp value in rfc850-date format, which uses a
         // two-digit year, MUST interpret a timestamp that appears to be more
-        // than 50 years in the future as representing the most recent year in the
-        // past that had the same last two digits.
+        // than 50 years in the future as representing the most recent year
+        // in the past that had the same last two digits.
         final currentYear = DateTime.now().toUtc().year;
         var year = currentYear - currentYear % 100 + _parseInt(scanner, 2);
         if (year > currentYear + 50) year -= 100;
