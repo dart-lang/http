f934af716aa7b50b7b0d538b0e1e29f70fdaf75a
Brian Quinlan <bquinlan@google.com>
2026-08-06 10:20:16 -0700

    Update main.dart

--- pkgs/http/example/main.dart
+++ pkgs/http/example/main.dart
@@ -9,10 +9,11 @@ import 'package:http/http.dart' as http;
 void main(List<String> arguments) async {
   // This example uses the pub.dev API to fetch details about package:http.
   // See https://pub.dev/help/api
+  final client = http.Client();
   var url = Uri.https('pub.dev', '/api/packages/http/score');
 
   // Await the http get response, then decode the json-formatted response.
-  var response = await http.get(url);
+  var response = await client.get(url);
   if (response.statusCode == 200) {
     var jsonResponse =
         convert.jsonDecode(response.body) as Map<String, dynamic>;
@@ -24,4 +25,5 @@ void main(List<String> arguments) async {
   } else {
     print('Request failed with status: ${response.statusCode}.');
   }
+  client.close();
 }
