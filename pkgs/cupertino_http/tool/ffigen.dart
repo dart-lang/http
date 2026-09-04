44c438fd62637a8efa050a7152e6d48297536ca5
Liam Appelbe <liama@google.com>
2026-09-04 13:04:56 +1000

    clean up

--- pkgs/cupertino_http/tool/ffigen.dart
+++ pkgs/cupertino_http/tool/ffigen.dart
@@ -6,112 +6,121 @@ import 'dart:io';
 
 import 'package:ffigen/ffigen.dart';
 
-FfiGenerator getConfig(Uri packageRoot) => FfiGenerator(
-  input: Input(
+FfiGenerator getConfig(Uri packageRoot) {
+  const headers = [
+    'NSURLCache.h',
+    'NSURLRequest.h',
+    'NSURLSession.h',
+    'NSURL.h',
+    'NSLock.h',
+    'NSProgress.h',
+    'NSURLResponse.h',
+    'NSHTTPCookieStorage.h',
+    'NSOperation.h',
+    'NSError.h',
+    'NSDictionary.h',
+  ];
+  final input = Input(
     entryPoints: [
-      for (final header in const [
-        'NSURLCache.h',
-        'NSURLRequest.h',
-        'NSURLSession.h',
-        'NSURL.h',
-        'NSLock.h',
-        'NSProgress.h',
-        'NSURLResponse.h',
-        'NSHTTPCookieStorage.h',
-        'NSOperation.h',
-        'NSError.h',
-        'NSDictionary.h',
-      ])
+      for (final header in headers)
         macSdkUri.resolve(
           'System/Library/Frameworks/Foundation.framework/Headers/$header',
         ),
     ],
-  ),
-  objectiveC: const ObjectiveC(),
-  output: Output(
-    dart: DartOutput(
-      path: packageRoot.resolve('lib/src/native_cupertino_bindings.dart'),
-    ),
-    objectiveCFile: packageRoot.resolve('src/native_cupertino_bindings.m'),
-    preamble: '''
+  );
+  const preamble = '''
 // ignore_for_file: always_specify_types
 // ignore_for_file: camel_case_types
 // ignore_for_file: non_constant_identifier_names
 // ignore_for_file: unused_element
 // ignore_for_file: unused_field
 // ignore_for_file: return_of_invalid_type
-''',
-    commentType: const CommentType(CommentStyle.any, CommentLength.full),
-  ),
-  visitors: [
-    Visitor(
-      objCInterface: (node) {
-        node.isIncluded = const {
-          'NSCondition',
-          'NSHTTPURLResponse',
-          'NSMutableURLRequest',
-          'NSOperationQueue',
-          'NSURLCache',
-          'NSURLRequest',
-          'NSURLResponse',
-          'NSURLSession',
-          'NSURLSessionConfiguration',
-          'NSURLSessionDownloadTask',
-          'NSURLSessionTask',
-          'NSURLSessionWebSocketMessage',
-          'NSURLSessionWebSocketTask',
-        }.contains(node.originalName);
-      },
-      objCMethod: (node) {
-        final interfaceRenames = const {
-          // TODO(brianquinlan): Remove this when
-          // https://github.com/dart-lang/native/issues/2419 is fixed.
-          'NSURLResponse': {
-            'initWithURL:MIMEType:expectedContentLength:textEncodingName:':
-                'initWithUrlAndMIMEType',
-          },
-          'NSHTTPURLResponse': {
-            'initWithURL:statusCode:HTTPVersion:headerFields:':
-                'initWithURLAndStatusCode',
-          },
-        }[node.parent.originalName];
-        if (interfaceRenames != null) {
-          final rename = interfaceRenames[node.originalName];
-          if (rename != null) {
-            node.name = rename;
-          }
-        }
-      },
-      objCProtocol: (node) {
-        node.isIncluded = const {
-          'NSURLSessionDataDelegate',
-          'NSURLSessionDownloadDelegate',
-          'NSURLSessionWebSocketDelegate',
-        }.contains(node.originalName);
-      },
-      enumClass: (node) {
-        final isIntEnum = const {
-          'NSURLSessionWebSocketCloseCode',
-        }.contains(node.originalName);
-        if (isIntEnum) {
-          node
-            ..isIncluded = true
-            ..style = EnumStyle.intConstants;
-        } else {
-          node.isIncluded = const {
-            'NSHTTPCookieAcceptPolicy',
-            'NSURLRequestCachePolicy',
-            'NSURLRequestNetworkServiceType',
-            'NSURLSessionMultipathServiceType',
-            'NSURLSessionResponseDisposition',
-            'NSURLSessionTaskState',
-            'NSURLSessionWebSocketMessageType',
-          }.contains(node.originalName);
-        }
-      },
+''';
+  final output = Output(
+    dart: DartOutput(
+      path: packageRoot.resolve('lib/src/native_cupertino_bindings.dart'),
     ),
-  ],
-);
+    objectiveCFile: packageRoot.resolve('src/native_cupertino_bindings.m'),
+    preamble: preamble,
+    commentType: const CommentType(CommentStyle.any, CommentLength.full),
+  );
+  return FfiGenerator(
+    input: input,
+    objectiveC: const ObjectiveC(),
+    output: output,
+    visitors: [
+      Visitor(
+        objCInterface: (node) {
+          const included = {
+            'NSCondition',
+            'NSHTTPURLResponse',
+            'NSMutableURLRequest',
+            'NSOperationQueue',
+            'NSURLCache',
+            'NSURLRequest',
+            'NSURLResponse',
+            'NSURLSession',
+            'NSURLSessionConfiguration',
+            'NSURLSessionDownloadTask',
+            'NSURLSessionTask',
+            'NSURLSessionWebSocketMessage',
+            'NSURLSessionWebSocketTask',
+          };
+          node.isIncluded = included.contains(node.originalName);
+        },
+        objCMethod: (node) {
+          const memberRenames = {
+            // TODO(brianquinlan): Remove this when
+            // https://github.com/dart-lang/native/issues/2419 is fixed.
+            'NSURLResponse': {
+              'initWithURL:MIMEType:expectedContentLength:textEncodingName:':
+                  'initWithUrlAndMIMEType',
+            },
+            'NSHTTPURLResponse': {
+              'initWithURL:statusCode:HTTPVersion:headerFields:':
+                  'initWithURLAndStatusCode',
+            },
+          };
+          final interfaceRenames = memberRenames[node.parent.originalName];
+          if (interfaceRenames != null) {
+            final rename = interfaceRenames[node.originalName];
+            if (rename != null) {
+              node.name = rename;
+            }
+          }
+        },
+        objCProtocol: (node) {
+          const included = {
+            'NSURLSessionDataDelegate',
+            'NSURLSessionDownloadDelegate',
+            'NSURLSessionWebSocketDelegate',
+          };
+          node.isIncluded = included.contains(node.originalName);
+        },
+        enumClass: (node) {
+          const intEnums = {'NSURLSessionWebSocketCloseCode'};
+          final isIntEnum = intEnums.contains(node.originalName);
+          if (isIntEnum) {
+            node
+              ..isIncluded = true
+              ..style = EnumStyle.intConstants;
+          } else {
+            const included = {
+              'NSHTTPCookieAcceptPolicy',
+              'NSURLRequestCachePolicy',
+              'NSURLRequestNetworkServiceType',
+              'NSURLSessionMultipathServiceType',
+              'NSURLSessionResponseDisposition',
+              'NSURLSessionTaskState',
+              'NSURLSessionWebSocketMessageType',
+            };
+            node.isIncluded = included.contains(node.originalName);
+          }
+        },
+      ),
+    ],
+  );
+}
 
 Uri _findPackageRoot() {
   if (Platform.script.isScheme('file')) {
